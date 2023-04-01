202274a631355f29f0bc4c103f4ecadd075e7b4b
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-01 18:19:03 +0200

    Add card

--- resources/views/comics-list.blade.php
+++ resources/views/comics-list.blade.php
@@ -5,7 +5,26 @@
 @section('content')
     <section >
         <div class="container">
-            @dump($comics)
+            <div class="row gap-3">
+
+                @forelse($comics as $comic)
+                <div class="col-3">
+                    <div class="card h-100 " >
+                        <div class="card-body d-flex flex-column">
+                          <h5 class="card-title">{{ $comic['title'] }}</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">{{ $comic['series']}}</h6>
+                          <p class="card-text">{{ $comic['sale_date']}}</p>
+                          <a href="#" class="card-link mt-auto">Vai al dettaglio</a>
+                        </div>
+                      </div>
+                </div>
+                @empty
+                <div class="col-12">
+                    <h2>Nessun comic nella lista</h2>
+                </div>
+    
+                @endforelse
+            </div>
         </div>
     </section>
 @endsection 
