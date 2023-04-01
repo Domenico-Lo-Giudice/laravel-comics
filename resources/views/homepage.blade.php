6c0f237920b5042885114ddba3bf8d42661cfe91
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-01 17:14:25 +0200

    Add layouts

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> {{ env('APP_NAME') }} - Homepage </title>
+@extends('layouts.app')
 
-    @vite('resources/js/app.js')
-    
-
-</head>
-<body>
-    <h1>Title</h1>
-
-    @dump($comics);
-</body>
-</html>
+@section('title')
+    Homepage
+@endsection
